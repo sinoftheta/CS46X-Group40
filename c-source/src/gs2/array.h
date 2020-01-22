8122f5b5e1efc07e0d76426e7a9fd9615f1bc309
unknown <ian.roboto@gmail.com>
2020-01-22 02:15:28 -0800

    removed min/max prototypes from array.h

--- c-source/src/gs2/array.h
+++ c-source/src/gs2/array.h
@@ -5,9 +5,6 @@
 #include "../capstone/Matrix.h"
 #include "../capstone/MathUtil.h"
 
-
 void gs2Array(Matrix*, Array*, int, int, int, int, int, int, int, int*);
 
-int min(int, int);
-int max(int, int);
 #endif /*__CAP_GS2_ARRAY_H__*/
