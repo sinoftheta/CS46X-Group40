d236ade71ae9fc45c8712acae3b332841122259c
unknown <ian.roboto@gmail.com>
2020-01-22 05:49:16 -0800

    finished sband.c

--- c-source/src/gs2/sband.c
+++ c-source/src/gs2/sband.c
@@ -2,7 +2,7 @@
 
 void gs2Sband(Matrix* s, Array* p, Array* u, int n, int nb, int ndim, int mdim, int nu){
 
-    int j, k1, ii;
+    int j, k1, k2, ii, kk;
     double sum, ski;
 
     for(int i = 1; i <= n; i++){
@@ -26,6 +26,30 @@ void gs2Sband(Matrix* s, Array* p, Array* u, int n, int nb, int ndim, int mdim, 
             
             *arrayAt(u, i) = sum* *matrixAt(s, i, 1);
 
-    }
+            //30
+            for(int i1 = 1; i1 <= n; i1++){
 
+            i = n - i1 + 1;
+            j = i + nb - 1;
+
+            if(j > n) j = n;
+
+            sum = *arrayAt(u, i);
+            k2 = i + 1;
+            
+            if(!(k2 > j)){ // if(k2 > j) go to 50
+
+                for(int k = k2; k <= j; k++){
+
+                    kk = k - i + 1;
+                    ski = *matrixAt(s, i, kk);
+                    if(ski != 0.0) sum = sum - ski * *arrayAt(u, k);
+                    *arrayAt(u, i) = sum * *matrixAt(s, i, 1);
+                }
+            }
+            //50
+
+            //60
+        }
+    }
 }
