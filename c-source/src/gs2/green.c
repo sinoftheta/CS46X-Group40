eceeb874dc1f80d6564d089d440a39bea554123c
bhambleton <hambletb@flip1.engr.oregonstate.edu>
2020-01-25 15:10:00 -0800

    Comment out unused variables

--- c-source/src/gs2/green.c
+++ c-source/src/gs2/green.c
@@ -4,8 +4,14 @@ void gs2Green(Array* x, Array* y, Array* detj, Array* ag, Matrix* in,
               Array* kf, Array* jd, Array* ieq, int* ms, int me, int np,
               int l, int* istop){
 
-      int kn[2], jn[4], js[4];
-      double alf[2], g[4][4], dlx[4][4];
+      int kn[2], js[4];
+      // int jn[4]; written to but never read
+      // all writes to jn have been commented out
+      double dlx[4][4];
+      // double g[4][4], alf[2]; written to but never read in this function
+      //	all writes to g have been commented out
+      //	all read/writes to alf commented out
+      //		alf only read into g
       int ic = 0, ii = 0, i = 0;
 
       kn[0] = 0;
@@ -31,7 +37,7 @@ void gs2Green(Array* x, Array* y, Array* detj, Array* ag, Matrix* in,
             }
 
             int knk = kn[k];
-            jn[ii] = *arrayAt(jd, knk);
+            //jn[ii] = *arrayAt(jd, knk);
             js[ii] = *arrayAt(jd, knk);
             *arrayAt(ieq, ii) = i;
             ii++;
@@ -49,7 +55,8 @@ void gs2Green(Array* x, Array* y, Array* detj, Array* ag, Matrix* in,
           for(int j = 0; j < 2; j++){ // changed loop from j = 1; j <= 2
             ip++;
             id++;
-            jn[ip] = *matrixAt(in, id, l);
+	    // Never used by the function
+            // jn[ip] = *matrixAt(in, id, l);
             if (*matrixAt(in, id, l) != 0){
               ii++;
               js[ii] = *matrixAt(in, id, l);  // will probably seg fault or access random mem not in js[]
@@ -74,29 +81,32 @@ void gs2Green(Array* x, Array* y, Array* detj, Array* ag, Matrix* in,
             if (np > 2){
               xi = *arrayAt(ag, k+2);
             }
-            double xi1 = 1.0 - xi;
+            
+	    // xi1 commented out because unused
+	    // double xi1 = 1.0 - xi;
             double xi2 = 1.0 + xi;
 
             // Corner node shape function, basic part
-            alf[0] = 0.5 * xi1;
-            alf[1] = 0.5 * xi2;
+            // 		Commented to out because only used by g[][]
+	    //alf[0] = 0.5 * xi1;
+            //alf[1] = 0.5 * xi2;
 
             // Determine if side is linear, quadratic, or cubic
             if ((*ms) == 3){
               // quadratic side
-              g[0][k] = (-xi * alf[0]);
-              g[1][k] = (xi * alf[1]);
-              g[2][k] = (1.0 - (xi * xi));
+              //g[0][k] = (-xi * alf[0]);
+              //g[1][k] = (xi * alf[1]);
+              //g[2][k] = (1.0 - (xi * xi));
               dlx[0][k] = xi - 0.5;
               dlx[1][k] = xi + 0.5;
               dlx[2][k] = -2.0 * xi;
             }
             else if ((*ms) == 4){
               // Cubic side
-              g[0][k] = alf[0] * (1.125 * xi2 - 0.125);
-              g[1][k] = alf[1] * (1.125 * xi2 - 0.125);
-              g[2][k] = 0.5625 * (1.0 - xi2) * (1.0 - 3.0 * xi);
-              g[3][k] = 0.5625 * (1.0 - xi2) * (1.0 + 3.0 * xi);
+              //g[0][k] = alf[0] * (1.125 * xi2 - 0.125);
+              //g[1][k] = alf[1] * (1.125 * xi2 - 0.125);
+              //g[2][k] = 0.5625 * (1.0 - xi2) * (1.0 - 3.0 * xi);
+              //g[3][k] = 0.5625 * (1.0 - xi2) * (1.0 + 3.0 * xi);
               dlx[0][k] = 0.0625 * (18.0 * xi - 27.0 * xi2 + 1.0);
               dlx[1][k] = 0.0625 * (18.0 * xi + 27.0 * xi2 - 1.0);
               dlx[2][k] = 0.5625 * (9.0 * xi2 - 2.0 * xi - 3.0);
@@ -104,8 +114,8 @@ void gs2Green(Array* x, Array* y, Array* detj, Array* ag, Matrix* in,
             }
             else{
               // Linear side
-              g[0][k] = alf[0];
-              g[1][k] = alf[1];
+              //g[0][k] = alf[0];
+              //g[1][k] = alf[1];
               dlx[0][k] = -0.5;
               dlx[1][k] = 0.5;
             }
