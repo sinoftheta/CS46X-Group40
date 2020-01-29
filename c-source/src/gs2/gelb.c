23e72172f418edaa08d5391016af863acc57ec89
unknown <ian.roboto@gmail.com>
2020-01-29 04:12:22 -0800

    added to gelb.c

--- c-source/src/gs2/gelb.c
+++ c-source/src/gs2/gelb.c
@@ -1,4 +1,4 @@
-#include "gleb.h"
+#include "gelb.h"
 
 //label 47, error return 
 #define ERR {*ier = -1; return;}
@@ -7,6 +7,8 @@ void gs2Gelb(
     Array* r, Array* a, int m, int n, 
     int mud, int mld, int eps, int* ier
 ){
+    double piv, tb, tol;
+    int j, jj, kst, ic, idst, id, ilr, ii;
 
     arrayAssertNotNull(r, "array 'r' NULL in gs2Gleb");
     arrayAssertNotNull(a, "array 'a' NULL in gs2Gleb");
@@ -40,9 +42,6 @@ void gs2Gelb(
 
     //move elements backwars and search for absolutely greatest element
 
-    double piv, tb;
-    int j, jj, kst, ic;
-
     ier = 0;
     piv = 0.0;
     if(mld <= 0){
@@ -50,7 +49,7 @@ void gs2Gelb(
         jj = ma;
         j - ma - mz;
         kst = j;
-        for(int k = 1; k <= kst; ++k){
+        for(int iter = 1; iter <= kst; ++iter){
 
             tb = *arrayAt(a, j);
             *arrayAt(a, jj) = tb;
@@ -63,12 +62,50 @@ void gs2Gelb(
         // insert zeros in first mu rows (not necessary in case mz = 0)
         if(mz <= 0){
             jj = 1;
-            j = 1+ mz;
+            j = 1 + mz;
             ic = 1 + mud;
 
+            for(int i = 1; i <= mu; ++i){
+                for(int k = 1; k <= mc; ++k){
+                    *arrayAt(a, jj) = 0.0;
+                    if(k - ic <= 0 ){
+                        *arrayAt(a, jj) = *arrayAt(a, j);
+                        ++j;
+                    }
+                    ++jj;
+                    ++ic;
+                }
+            }
         }
     }
     //14
+
+    // generate test value for singularity
+    tol = eps * piv;
+
+    // start decomposition loop
+    kst = 1;
+    idst = mc;
+    ic = mc - 1;
+
+    for(int k = 0; k <= m; ++k){
+        if(k - mr - 1 > 0 ) --idst;
+        id = idst;
+        ilr = k + mld;
+        if(ilr - m > 0) ilr = m;
+        ii = kst;
+
+        // pivot search in first column (row indexes from i = k up to i = ilr)
+        piv = 0.0;
+
+        for(int i = k; i <= ilr; ++i){
+            
+        }
+        // 22
+
+    }
+    // 38
+
 
 
 
