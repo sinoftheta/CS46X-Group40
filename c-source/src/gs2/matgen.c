50aca04b2596bfaeb20f7a629f2d2c8d563051e7
Ivan Timothy Halim <ivan.ith94@gmail.com>
2020-01-29 22:16:54 -0800

    Add gs2State

--- c-source/src/gs2/matgen.c
+++ c-source/src/gs2/matgen.c
@@ -5,14 +5,7 @@
  * Purpose: To perform surface integrations
  */
 
-void gs2Matgen1(Matrix* pe, Matrix* se, Array* q, Array* qp, int i, int j) {
-    
-    /* 
-     * Global variables:
-     * - h1
-     * - h2
-     * - np
-     */
+void gs2Matgen1(gs2State* state, Matrix* pe, Matrix* se, Array* q, Array* qp, int i, int j) {
 
     arrayAssertNotNull(q, "Array 'q' NULL in gs2Matgen!");
     arrayAssertNotNull(qp, "Array 'qp' NULL in gs2Matgen!");
@@ -21,11 +14,11 @@ void gs2Matgen1(Matrix* pe, Matrix* se, Array* q, Array* qp, int i, int j) {
     
     double h11, h12, h22;
 
-    h11 = h1 * h1;
-    h12 = h1 * h2;
-    h22 = h2 * h2;
+    h11 = state->h1 * state->h1;
+    h12 = state->h1 * state->h2;
+    h22 = state->h2 * state->h2;
 
-    if (np == 4) {
+    if (state->np == 4) {
         *matrixAt(se, i, j) = h11 * (*arrayAt(q, 0) + *arrayAt(q, 3) + *arrayAt(q, 12) + *arrayAt(q, 15)) \
                             + h12 * (*arrayAt(q, 1) + *arrayAt(q, 2) + *arrayAt(q, 4) + *arrayAt(q, 7) + *arrayAt(q, 8) + *arrayAt(q, 11) + *arrayAt(q, 13) + *arrayAt(q, 14)) \
                             + h22 * (*arrayAt(q, 5) + *arrayAt(q, 6) + *arrayAt(q, 9) + *arrayAt(q, 10));
@@ -39,25 +32,18 @@ void gs2Matgen1(Matrix* pe, Matrix* se, Array* q, Array* qp, int i, int j) {
 
 }
 
-void gs2Matgen2(Array* srcr, Array* srcrt, int i) {
-
-    /* 
-     * Global variables:
-     * - h1
-     * - h2
-     * - np
-     */
+void gs2Matgen2(gs2State* state, Array* srcr, Array* srcrt, int i) {
 
     arrayAssertNotNull(srcr, "Array 'srcr' NULL in gs2Matgen!");
     arrayAssertNotNull(srcrt, "Array 'srcrt' NULL in gs2Matgen!");
 
     double h11, h12, h22;
 
-    h11 = h1 * h1;
-    h12 = h1 * h2;
-    h22 = h2 * h2;
+    h11 = state->h1 * state->h1;
+    h12 = state->h1 * state->h2;
+    h22 = state->h2 * state->h2;
 
-    if (np == 4) {
+    if (state->np == 4) {
         *arrayAt(srcr, i) = h11 * (*arrayAt(srcrt, 0) + *arrayAt(srcrt, 3) + *arrayAt(srcrt, 12) + *arrayAt(srcrt, 15)) \
                           + h12 * (*arrayAt(srcrt, 1) + *arrayAt(srcrt, 2) + *arrayAt(srcrt, 4) + *arrayAt(srcrt, 7) + *arrayAt(srcrt, 8) + *arrayAt(srcrt, 11) + *arrayAt(srcrt, 13) + *arrayAt(srcrt, 14)) \
                           + h22 * (*arrayAt(srcrt, 5) + *arrayAt(srcrt, 6) + *arrayAt(srcrt, 9) + *arrayAt(srcrt, 10));
@@ -66,12 +52,7 @@ void gs2Matgen2(Array* srcr, Array* srcrt, int i) {
     }
 }
 
-void gs2Matgen3(Matrix* pe, Matrix* se) {
-
-    /* 
-     * Global variables:
-     * - m
-     */
+void gs2Matgen3(Matrix* pe, Matrix* se, int m) {
 
     matrixAssertNotNull(pe, "Matrix 'pe' NULL in gs2Shape!");
     matrixAssertNotNull(se, "Matrix 'se' NULL in gs2Shape!");
