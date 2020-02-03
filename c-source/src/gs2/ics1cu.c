e6a356d3d6e4da2b21930696d8f9520093d3f2dc
dakota-osu <altond@oregonstate.edu>
2020-02-02 17:10:34 -0800

    maybe adds ics1cu

--- c-source/src/gs2/ics1cu.c
+++ c-source/src/gs2/ics1cu.c
@@ -1,5 +1,38 @@
 #include "ics1cu.h"
 
-void gs2ICS1CU() {
-    
+#include <gsl/gsl_interp.h>
+
+#include <stdio.h>
+
+void gs2ICS1CU(
+    Array* xs, 
+    Array* ys, 
+    int ispk, 
+    Matrix* cc,
+    int material,
+    int* ier
+) {
+
+    gsl_interp* interp = gsl_interp_alloc(gsl_interp_cspline, ispk);
+
+    *ier = gsl_interp_init(interp, xs->elements, ys->elements, xs->size);
+
+    gsl_interp_accel *acc = gsl_interp_accel_alloc();
+
+    // constant
+    double d = gsl_interp_eval(interp, xs->elements, ys->elements, 0.0, acc);
+    // linear
+    double c = gsl_interp_eval_deriv(interp, xs->elements, ys->elements, 0.0, acc);
+    // quadratic
+    double b = 0.5 * gsl_interp_eval_deriv2(interp, xs->elements, ys->elements, 0.0, acc);
+    // cubic
+    double a = gsl_interp_eval(interp, xs->elements, ys->elements, 1.0, acc) - b - c - d;
+
+   
+    *matrixAt(cc, 3, material) = a;
+    *matrixAt(cc, 2, material) = b;
+    *matrixAt(cc, 1, material) = c;
+
+    gsl_interp_accel_free(acc);
+    gsl_interp_free(interp);
 }
--- c-source/src/gs2/ics1cu.h
+++ c-source/src/gs2/ics1cu.h
@@ -1,8 +1,17 @@
 #ifndef __CAP_ICS1CU_H__
 #define __CAP_ICS1CU_H__
 
+#include "../capstone/Array.h"
+#include "../capstone/Matrix.h"
 
 
-void gs2ICS1CU();
+void gs2ICS1CU(
+    Array* xs, 
+    Array* ys, 
+    int ispk, 
+    Matrix* cc,
+    int material,
+    int* ier
+);
 
 #endif /* __CAP_ICS1CU_H__ */ 
