1677e983e106e3f75ce7ab39fc5a0cec40f273e5
Ivan Timothy Halim <ivan.ith94@gmail.com>
2020-02-03 20:22:14 -0800

    Revert to 1 indexing

--- c-source/src/gs2/matgen.c
+++ c-source/src/gs2/matgen.c
@@ -19,15 +19,15 @@ void gs2Matgen1(gs2State* state, Matrix* pe, Matrix* se, Array* q, Array* qp, in
     h22 = state->h2 * state->h2;
 
     if (state->np == 4) {
-        *matrixAt(se, i, j) = h11 * (*arrayAt(q, 0) + *arrayAt(q, 3) + *arrayAt(q, 12) + *arrayAt(q, 15)) \
-                            + h12 * (*arrayAt(q, 1) + *arrayAt(q, 2) + *arrayAt(q, 4) + *arrayAt(q, 7) + *arrayAt(q, 8) + *arrayAt(q, 11) + *arrayAt(q, 13) + *arrayAt(q, 14)) \
-                            + h22 * (*arrayAt(q, 5) + *arrayAt(q, 6) + *arrayAt(q, 9) + *arrayAt(q, 10));
-        *matrixAt(pe, i, j) = h11 * (*arrayAt(qp, 0) + *arrayAt(qp, 3) + *arrayAt(qp, 12) + *arrayAt(qp, 15)) \
-                            + h12 * (*arrayAt(qp, 1) + *arrayAt(qp, 2) + *arrayAt(qp, 4) + *arrayAt(qp, 7) + *arrayAt(qp, 8) + *arrayAt(qp, 11) + *arrayAt(qp, 13) + *arrayAt(qp, 14)) \
-                            + h22 * (*arrayAt(qp, 5) + *arrayAt(qp, 6) + *arrayAt(qp, 9) + *arrayAt(qp, 10));
+        *matrixAt(se, i, j) = h11 * (*arrayAt(q, 1) + *arrayAt(q, 4) + *arrayAt(q, 13) + *arrayAt(q, 16)) \
+                            + h12 * (*arrayAt(q, 2) + *arrayAt(q, 3) + *arrayAt(q, 5) + *arrayAt(q, 8) + *arrayAt(q, 9) + *arrayAt(q, 12) + *arrayAt(q, 14) + *arrayAt(q, 15)) \
+                            + h22 * (*arrayAt(q, 6) + *arrayAt(q, 7) + *arrayAt(q, 10) + *arrayAt(q, 11));
+        *matrixAt(pe, i, j) = h11 * (*arrayAt(qp, 1) + *arrayAt(qp, 4) + *arrayAt(qp, 13) + *arrayAt(qp, 16)) \
+                            + h12 * (*arrayAt(qp, 2) + *arrayAt(qp, 3) + *arrayAt(qp, 5) + *arrayAt(qp, 8) + *arrayAt(qp, 9) + *arrayAt(qp, 12) + *arrayAt(qp, 14) + *arrayAt(qp, 15)) \
+                            + h22 * (*arrayAt(qp, 6) + *arrayAt(qp, 7) + *arrayAt(qp, 10) + *arrayAt(qp, 11));
     } else {
-        *matrixAt(se, i, j) = *arrayAt(q, 0) + *arrayAt(q, 1) + *arrayAt(q, 2) + *arrayAt(q, 3);
-        *matrixAt(pe, i, j) = *arrayAt(qp, 0) + *arrayAt(qp, 1) + *arrayAt(qp, 2) + *arrayAt(qp, 3);
+        *matrixAt(se, i, j) = *arrayAt(q, 1) + *arrayAt(q, 2) + *arrayAt(q, 3) + *arrayAt(q, 4);
+        *matrixAt(pe, i, j) = *arrayAt(qp, 1) + *arrayAt(qp, 2) + *arrayAt(qp, 3) + *arrayAt(qp, 4);
     }
 
 }
@@ -44,11 +44,11 @@ void gs2Matgen2(gs2State* state, Array* srcr, Array* srcrt, int i) {
     h22 = state->h2 * state->h2;
 
     if (state->np == 4) {
-        *arrayAt(srcr, i) = h11 * (*arrayAt(srcrt, 0) + *arrayAt(srcrt, 3) + *arrayAt(srcrt, 12) + *arrayAt(srcrt, 15)) \
-                          + h12 * (*arrayAt(srcrt, 1) + *arrayAt(srcrt, 2) + *arrayAt(srcrt, 4) + *arrayAt(srcrt, 7) + *arrayAt(srcrt, 8) + *arrayAt(srcrt, 11) + *arrayAt(srcrt, 13) + *arrayAt(srcrt, 14)) \
-                          + h22 * (*arrayAt(srcrt, 5) + *arrayAt(srcrt, 6) + *arrayAt(srcrt, 9) + *arrayAt(srcrt, 10));
+        *arrayAt(srcr, i) = h11 * (*arrayAt(srcrt, 1) + *arrayAt(srcrt, 4) + *arrayAt(srcrt, 13) + *arrayAt(srcrt, 16)) \
+                          + h12 * (*arrayAt(srcrt, 2) + *arrayAt(srcrt, 3) + *arrayAt(srcrt, 5) + *arrayAt(srcrt, 8) + *arrayAt(srcrt, 9) + *arrayAt(srcrt, 12) + *arrayAt(srcrt, 14) + *arrayAt(srcrt, 15)) \
+                          + h22 * (*arrayAt(srcrt, 6) + *arrayAt(srcrt, 7) + *arrayAt(srcrt, 10) + *arrayAt(srcrt, 11));
     } else {
-        *arrayAt(srcr, i) = *arrayAt(srcrt, 0) + *arrayAt(srcrt, 1) + *arrayAt(srcrt, 2) + *arrayAt(srcrt, 3);
+        *arrayAt(srcr, i) = *arrayAt(srcrt, 1) + *arrayAt(srcrt, 2) + *arrayAt(srcrt, 3) + *arrayAt(srcrt, 4);
     }
 }
 
@@ -59,39 +59,39 @@ void gs2Matgen3(Matrix* pe, Matrix* se, int m) {
 
     int mm2, k1, k2, im, jm;
 
-    printf("\n\n\n\n          ELEMENT%4d     STIFFNESS MATRIX\n", l);
+    printf("\n\n\n\n          ELEMENT%4d     STIFFNESS MATRIX\n\n", l);
 
     mm2 = (m+7)/8*8 - 7;
 
-    for (k1 = 0; k1 < mm2; k1 += 8) {
+    for (k1 = 1; k1 <= mm2; k1 += 8) {
         k2 = k1 + 7;
-        if (k1 == mm2 - 1) {
-            k2 = m - 1;
+        if (k1 == mm2) {
+            k2 = m;
         }
-        for (im = 0; im < m; im++) {
+        for (im = 1; im <= m; im++) {
             printf("%5d", im);
             for (jm = k1; jm <= k2; jm++) {
                 printf("%15.6f", *matrixAt(se, im, jm));
             }
-            printf("\n\n");
+            printf("\n");
         }
+        printf("\n\n\n");
     }
 
-    printf("\n\n\n\n          ELEMENT%4d     STORAGE MATRIX\n", l);
+    printf("\n\n\n\n          ELEMENT%4d     STORAGE MATRIX\n\n", l);
 
-    for (k1 = 0; k1 < mm2; k1 += 8) {
+    for (k1 = 1; k1 <= mm2; k1 += 8) {
         k2 = k1 + 7;
-        if (k1 == mm2 - 1) {
-            k2 = m - 1;
+        if (k1 == mm2) {
+            k2 = m;
         }
-        for (im = 0; im < m; im++) {
+        for (im = 1; im <= m; im++) {
             printf("%5d", im);
             for (jm = k1; jm <= k2; jm++) {
                 printf("%15.6f", *matrixAt(pe, im, jm));
             }
-            printf("\n\n");
+            printf("\n");
         }
+        printf("\n\n\n");
     }
-
-
 }
