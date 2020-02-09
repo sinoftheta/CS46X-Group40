cbd9d1bea3807e136aee60927c2a1556a15b4ccb
dakota-osu <altond@oregonstate.edu>
2020-02-08 16:14:26 -0800

    fixed issues

--- c-source/src/gs2/sband.c
+++ c-source/src/gs2/sband.c
@@ -1,60 +1,59 @@
 #include "sband.h"
 
-void gs2Sband(Matrix* s, Array* p, Array* u, int n, int nb){
+void gs2Sband(Matrix* s, Array* p, Array* u, int n, int nb) {
 
-    int j, k1, k2, ii, kk;
-    double sum, ski;
+    matrixAssertNotNull(s, "matrix s in gs2Sband is null");
+    arrayAssertNotNull(p, "array p in gs2Sband is null");
+    arrayAssertNotNull(u, "array u in gs2Sband is null");
 
     for(int i = 1; i <= n; i++){
 
-        matrixAssertNotNull(s, "matrix s in gs2Sband is null");
-        arrayAssertNotNull(p, "array p in gs2Sband is null");
-        arrayAssertNotNull(u, "array u in gs2Sband is null");
+        int j = i - nb + 1;
+        if((i  + 1) <= nb) 
+            j = 1;
 
-        j = i - nb + 1;
-        if((i  + 1) <= nb) j = 1;
-        sum = *arrayAt(p, i);
-        k1 = i - 1;
+        double sum = *arrayAt(p, i);
+        int k1 = i - 1;
 
-        if(!(j > k1)){ // if(j > k1) go to 20
+        if (j <= k1) { 
 
             for(int k = j; k <= k1; k++){
 
-                ii = i - k + 1;
-                ski = *matrixAt(s, k, ii);
+                int ii = i - k + 1;
+                double ski = *matrixAt(s, k, ii);
 
                 //10
-                if(ski != 0.0) sum = sum - ski * *arrayAt(u, k);
+                if(ski != 0.0) 
+                    sum = sum - ski * *arrayAt(u, k);
             }
         }
         //20
             
         *arrayAt(u, i) = sum* *matrixAt(s, i, 1);
+    }
 
-        //30
-        for(int i1 = 1; i1 <= n; i1++){
+    for(int i1 = 1; i1 <= n; i1++){
 
-            i = n - i1 + 1;
-            j = i + nb - 1;
+        int i = n - i1 + 1;
+        int j = i + nb - 1;
 
-            if(j > n) j = n;
+        if(j > n) 
+            j = n;
 
-            sum = *arrayAt(u, i);
-            k2 = i + 1;
-            
-            if(!(k2 > j)){ // if(k2 > j) go to 50
+        double sum = *arrayAt(u, i);
+        int k2 = i + 1;
+        
+        if(k2 <= j){ 
 
-                for(int k = k2; k <= j; k++){
+            for(int k = k2; k <= j; k++){
 
-                    kk = k - i + 1;
-                    ski = *matrixAt(s, i, kk);
-                    if(ski != 0.0) sum = sum - ski * *arrayAt(u, k);
-                    *arrayAt(u, i) = sum * *matrixAt(s, i, 1);
-                }
+                int kk = k - i + 1;
+                double ski = *matrixAt(s, i, kk);
+                if(ski != 0.0) 
+                    sum = sum - ski * *arrayAt(u, k);
+                
             }
-            //50
-
-        //60
+            *arrayAt(u, i) = sum * *matrixAt(s, i, 1);
         }
     }
 }
