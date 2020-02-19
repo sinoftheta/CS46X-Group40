18bec22ce20ab3ec73fe735e6a44e84b29c9fece
dakota-osu <altond@oregonstate.edu>
2020-02-19 10:48:40 -0800

    adds model for seepage face

---
+++ python/gui/seepage_face/SeepageFaceModel.py
@@ -0,0 +1,20 @@
+
+
+class SeepageFace:
+    def __init__(self, seepageFaceID = 0):
+        self.seepageFaceID = seepageFaceID
+
+        # the elements in this list will need to be kept in sync
+        # with the nodes page
+        self.diricheltNodes = []
+
+        # the elements in this list will need to be kept in sync
+        # with the nodes page
+        self.nuemannNodes = []
+
+
+    def getNumberOfDiricheltNodes(self):
+        return len(diricheltNodes)
+
+
+    
