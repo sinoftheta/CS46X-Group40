e737ab3908eda42ffc6c4c5713bd4fd6531a4806
IanBand <ian.roboto@gmail.com>
2020-03-06 13:32:41 -0800

    multipliers default value now 1.0

--- python/gui/multipliers/MultipliersModel.py
+++ python/gui/multipliers/MultipliersModel.py
@@ -1,19 +1,19 @@
 class MultipliersModel:
     def __init__(self):
-        self.AFMOBX = 0.0
-        self.AFMOBY = 0.0
-        self.APOR = 0.0
-        self.AELONG = 0.0
-        self.AETRANS = 0.0
-        self.APHII = 0.0
-        self.ACONCI = 0.0
-        self.XFACT = 0.0
-        self.YFACT = 0.0
-        self.ATETA = 0.0
-        self.AAL = 0.0
-        self.AKD = 0.0
-        self.ALAM = 0.0
-        self.ARHO = 0.0
+        self.AFMOBX = 1.0
+        self.AFMOBY = 1.0
+        self.APOR = 1.0
+        self.AELONG = 1.0
+        self.AETRANS = 1.0
+        self.APHII = 1.0
+        self.ACONCI = 1.0
+        self.XFACT = 1.0
+        self.YFACT = 1.0
+        self.ATETA = 1.0
+        self.AAL = 1.0
+        self.AKD = 1.0
+        self.ALAM = 1.0
+        self.ARHO = 1.0
     
     #generic mutator
     def setVal(self, key, val):
