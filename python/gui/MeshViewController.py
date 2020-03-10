8019cca6a29a23477ddfa847ece5a4bf2ef999c1
Ivan Timothy Halim <ivan.ith94@gmail.com>
2020-03-10 01:30:15 -0700

    Fix error

--- python/gui/MeshViewController.py
+++ python/gui/MeshViewController.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
 from .SimulationController import GS2CallbackListener
+from gs2 import types
 
 class MeshViewController(QGroupBox, GS2CallbackListener):
     def __init__(self):
@@ -21,15 +22,51 @@ class MeshViewController(QGroupBox, GS2CallbackListener):
         self.setLayout(self.layout)
 
     def createMesh(self, gs2, state):
-        vertices = np.array([[gs2.arrayAt(byref(state.x, i)), gs2.arrayAt(byref(state.y, i))] for i in range(1, state.nn + 1)])
-        faces = [[gs2.matrixAt(byref(state.in, i, j)) - 1 for j in range(1, state.memoryRequirements.maxne + 1) if gs2.matrixAt(byref(state.in, i, j)) > 0] for i in range(1, state.ne + 1)]
+        # vertices = np.array([[gs2.arrayAt(byref(state.x), i), gs2.arrayAt(byref(state.y), i)] for i in range(1, state.nn + 1)])
+
+        vertices = []
+        for i in range(1, state.nn + 1):
+            x = cast(gs2.arrayAt(byref(state.x), i), POINTER(c_double)).contents.value
+            y = cast(gs2.arrayAt(byref(state.y), i), POINTER(c_double)).contents.value
+            vertices.append([x, y])
+        vertices = np.array(vertices)
+        
+
+        # faces = [[gs2.matrixAt(byref(state.in), i, j) - 1 for j in range(1, state.memoryRequirements.maxne + 1)\
+        #          if gs2.matrixAt(byref(state.in), i, j) > 0] for i in range(1, state.ne + 1)]
+
+        faces = []
+        for i in range(1, state.ne + 1):
+            incidences = []
+            for j in range(1, state.memoryRequirements.maxne + 1):
+                inc = cast(gs2.matrixAt(byref(state.in), i, j, POINTER(c_double)).contents.value
+                if inc > 0:
+                    incidences.append(inc - 1)
+            faces.append(incidences)
+
+
         faces = [[len(row)] + row for row in faces]
         faces = np.hstack(faces)
         self.mesh = pv.PolyData(vertices, faces)
         self.vtk_widget.add_mesh(self.mesh)
 
     def onCallback(self, gs2, state):
+        
         if self.mesh is None:
             self.createMesh(gs2, state)
-        self.mesh.point_arrays['Pressure Head'] = np.array([gs2.arrayAt(byref(state.phi), i) for i in range(1, state.nn + 1)])
-        self.mesh.point_arrays['Concentration'] = np.array([gs2.arrayAt(byref(state.conc), i) for i in range(1, state.nn + 1)])
+
+        # self.mesh.point_arrays['Pressure Head'] = np.array([gs2.arrayAt(byref(state.phi), i) for i in range(1, state.nn + 1)])
+
+        head = []
+        for i in range(1, state.nn + 1):
+            h = cast(gs2.arrayAt(byref(state.phi), i), POINTER(c_double)).contents.value
+            head.append(h)
+        self.mesh.point_arrays['Pressure Head'] = np.array(head)
+
+        # self.mesh.point_arrays['Concentration'] = np.array([gs2.arrayAt(byref(state.conc), i) for i in range(1, state.nn + 1)])
+
+        conc = []
+        for i in range(1, state.nn + 1):
+            c = cast(gs2.arrayAt(byref(state.conc), i), POINTER(c_double)).contents.value
+            conc.append(c)
+        self.mesh.point_arrays['Concentration'] = np.array(conc)
