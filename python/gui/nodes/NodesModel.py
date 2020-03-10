bb85d1976e723e2935df07d20b854364a62d5581
unknown <ian.roboto@gmail.com>
2020-03-09 20:38:40 -0700

    worked on NodesModel

--- python/gui/nodes/NodesModel.py
+++ python/gui/nodes/NodesModel.py
@@ -1,14 +1,3 @@
-from ..parameters.ParametersModel import parametersModel
-
-class NodesModel():
-    def __init(self)__:
-        self.nodes = []
-        for i in range(0, parametersModel.NN.getData):
-            self.nodes.append(NodeModel(i + 1))
-
-    # updates the nodes array to have the same length as NN 
-    def _updateNodes(self):
-
 class NodeModel():
     def __init(self, NodeNum)__:
         self.I = NodeNum
@@ -18,3 +7,21 @@ class NodeModel():
         self.PHII = 0.0
         self.boundary = ""
 
+class NodesModel():
+    def __init(self, parametersModel)__:
+        self.nodes = []
+        
+        for i in range(0, parametersModel.NN.getData):
+            self.nodes.append(NodeModel(i + 1))
+
+        parametersModel.NN.connectObserver(self._updateNodes)
+
+    # updates the nodes array to have the same length as NN 
+    def _updateNodes(self, newNumNodes):
+        if(newNumNodes > len(self.nodes)):
+            while(len(self.nodes) != newNumNodes):
+                self.nodes.append(NodeModel(len(self.nodes) + 1))
+        elif(newNumNodes < len(self.nodes)):
+            while(len(self.nodes) != newNumNodes):
+                self.nodes.remove(len(self.nodes) + 1)
+        
