1dd5819ebf7580e474a585c93050934d8ae1c008
Ivan Timothy Halim <ivan.ith94@gmail.com>
2020-03-10 02:26:43 -0700

    Add self.gs2
    onCallback() requires the functions arrayAt, matrixAt to access the arrays and matrices in state, but onCallback() is supposed to only accept state. Therefore, I propose to add the attribute self.gs2 in the MeshViewController class.

--- python/gui/MeshViewController.py
+++ python/gui/MeshViewController.py
@@ -9,8 +9,9 @@ from .SimulationController import GS2CallbackListener
 from gs2 import types
 
 class MeshViewController(QGroupBox, GS2CallbackListener):
-    def __init__(self):
+    def __init__(self, gs2):
         super(MeshViewController, self).__init__('Mesh')
+        self.gs2 = gs2
         self.layout = QVBoxLayout()
 
         self.frame = QFrame()
@@ -21,21 +22,21 @@ class MeshViewController(QGroupBox, GS2CallbackListener):
         self.layout.addWidget(self.vtk_widget)
         self.setLayout(self.layout)
 
-    def createMesh(self, gs2, state):
+    def createMesh(self, state):
 
         vertices = np.array([
             [
-                cast(gs2.arrayAt(byref(state.x), i), POINTER(c_double)).contents.value,
-                cast(gs2.arrayAt(byref(state.y), i), POINTER(c_double)).contents.value
+                cast(self.gs2.arrayAt(byref(state.x), i), POINTER(c_double)).contents.value,
+                cast(self.gs2.arrayAt(byref(state.y), i), POINTER(c_double)).contents.value
             ]
             for i in range(1, state.nn + 1)
         ])
 
         faces = [
             [
-                cast(gs2.matrixAt(byref(state.in), i, j, POINTER(c_double)).contents.value - 1
+                cast(self.gs2.matrixAt(byref(state.in), i, j, POINTER(c_double)).contents.value - 1
                 for j in range(1, state.memoryRequirements.maxne + 1)
-                if cast(gs2.matrixAt(byref(state.in), i, j, POINTER(c_double)).contents.value > 0
+                if cast(self.gs2.matrixAt(byref(state.in), i, j, POINTER(c_double)).contents.value > 0
             ]
             for i in range(1, state.ne + 1)
         ]
@@ -45,17 +46,17 @@ class MeshViewController(QGroupBox, GS2CallbackListener):
         self.mesh = pv.PolyData(vertices, faces)
         self.vtk_widget.add_mesh(self.mesh)
 
-    def onCallback(self, gs2, state):
+    def onCallback(self, state):
         
         if self.mesh is None:
-            self.createMesh(gs2, state)
+            self.createMesh(self.gs2, state)
 
         self.mesh.point_arrays['Pressure Head'] = np.array([
-            cast(gs2.arrayAt(byref(state.phi), i), POINTER(c_double)).contents.value
+            cast(self.gs2.arrayAt(byref(state.phi), i), POINTER(c_double)).contents.value
             for i in range(1, state.nn + 1)
         ])
 
         self.mesh.point_arrays['Concentration'] = np.array([
-            cast(gs2.arrayAt(byref(state.conc), i), POINTER(c_double)).contents.value
+            cast(self.gs2.arrayAt(byref(state.conc), i), POINTER(c_double)).contents.value
             for i in range(1, state.nn + 1)
         ])
